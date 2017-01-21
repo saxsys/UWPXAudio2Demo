44c43490885684864c9a6e9b4a9e682e40ae4e9a
Knoedelbart <metronomerous@knoedelbart.de>
2017-01-22 00:19:50 +0100

    Initial Commit

---
+++ WindowsRuntimeComponent/AudioComponent.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "AudioComponent.h"
+
+using namespace WindowsRuntimeComponent;
+using namespace Platform;
+
+AudioComponent::AudioComponent()
+{
+	XAudio2Create(&xaudio_engine);
+	xaudio_engine->CreateMasteringVoice(&xaudio_master);
+
+	ZeroMemory(&xaudio_buffer, sizeof(xaudio_buffer));
+	xaudio_buffer.AudioBytes = BUFFERSIZE;
+
+	WAVEFORMATEX waveformat;
+	waveformat.nChannels = 1;
+	waveformat.nSamplesPerSec = SAMPLERATE;
+	waveformat.wBitsPerSample = 8;
+	waveformat.nAvgBytesPerSec = SAMPLERATE;
+	waveformat.wFormatTag = WAVE_FORMAT_PCM;
+	waveformat.nBlockAlign = 1;
+	waveformat.cbSize = 0;
+
+	xaudio_engine->CreateSourceVoice(&xaudio_source, &waveformat, 0, XAUDIO2_DEFAULT_FREQ_RATIO, NULL, NULL, NULL);
+	xaudio_source->Start();
+
+	sounddata = new BYTE[BUFFERSIZE];
+	for (int i = 0; i < BUFFERSIZE; i++)
+	{
+		double v = sin(2 * PI * i * (440.0 / SAMPLERATE));
+		sounddata[i] = (BYTE)(v * 128) + 128;
+	}
+
+	xaudio_buffer.pAudioData = sounddata;
+}
+
+void AudioComponent::PlayTone()
+{
+	xaudio_source->SubmitSourceBuffer(&xaudio_buffer);
+}
+
+AudioComponent::~AudioComponent()
+{
+	xaudio_engine->Release();
+	delete sounddata;
+}
---
+++ WindowsRuntimeComponent/AudioComponent.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <xaudio2.h>
+
+#define PI 3.14159265358979323846f
+#define SAMPLERATE 44100  
+#define BUFFERSIZE 22500
+
+namespace WindowsRuntimeComponent
+{
+	public ref class AudioComponent sealed
+	{
+	public:
+		AudioComponent();
+		virtual ~AudioComponent();
+
+		void PlayTone();
+
+	private:
+		BYTE* sounddata;
+
+		IXAudio2* xaudio_engine;
+
+		XAUDIO2_BUFFER xaudio_buffer;
+		IXAudio2SourceVoice* xaudio_source;
+		IXAudio2MasteringVoice* xaudio_master;
+	};
+}
